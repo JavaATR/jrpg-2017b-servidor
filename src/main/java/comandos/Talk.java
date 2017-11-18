299ac176366c60fa39e44d08850dbc0123950e5f
Dan Moulton <dan.moulton@wildar.com>
2017-11-17 23:58:09 -0300

    Comienzo la funcionalidad de los trucos. Recibo y detecto cuando un jugador escribe un truco válido.

--- src/main/java/comandos/Talk.java
+++ src/main/java/comandos/Talk.java
@@ -1,6 +1,9 @@
 package comandos;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import mensajeria.Comando;
@@ -18,10 +21,15 @@ public class Talk extends ComandosServer {
 	 * <p>
 	 * <i>En caso de que no se pueda envíar el mensaje, se avisa.</i> <br>
 	 */
+	
+	List<String> trucos = new ArrayList<>(Arrays.asList("iddqd", "noclip", "bigdaddy", "tinydaddy", "war aint what it used to be"));
+	
 	@Override
 	public final void ejecutar() {
 		int idUser = 0;
 		int contador = 0;
+		int truco = 0;
+		
 		PaqueteMensaje paqueteMensaje = (PaqueteMensaje) (gson.fromJson(cadenaLeida, PaqueteMensaje.class));
 		if (!(paqueteMensaje.getUserReceptor() == null)) {
 			if (Servidor.mensajeAUsuario(paqueteMensaje)) {
@@ -44,24 +52,76 @@ public class Talk extends ComandosServer {
 			} else {
 				Servidor.log.append("No se envió el mensaje.\n");
 			}
-		} else {
+		} else { // Esto es cuando no se está hablando a nadie por privado. Acá se aplican los cheats
 			for (Map.Entry<Integer, PaquetePersonaje> personaje : Servidor.getPersonajesConectados().entrySet()) {
 				if (personaje.getValue().getNombre().equals(paqueteMensaje.getUserEmisor())) {
 					idUser = personaje.getValue().getId();
 					contador++;
 				}
 			}
-			for (EscuchaCliente conectado : Servidor.getClientesConectados()) {
-				if (conectado.getIdPersonaje() != idUser) {
-					try {
-						conectado.getSalida().writeObject(gson.toJson(paqueteMensaje));
-					} catch (IOException e) {
-						Servidor.log.append("Falló al intentar enviar mensaje a:"
-								+ conectado.getPaquetePersonaje().getId() + ".\n");
+			
+			truco = mensajeEsTruco(paqueteMensaje.getMensaje());
+			
+			if (truco != -1) {
+				switch (truco) {
+					case 0:
+						ponerPersonajeEnModoDios(idUser);
+						break;
+					case 1:
+						permitirPersonajeAtravesarParedes(idUser);
+						break;
+					case 2:
+						ponerPersonajeEnModoExtraFuerza(idUser);
+						break;
+					case 3:
+						ponerPersonajeEnModoMitadFuerza(idUser);
+						break;
+					case 4:
+						ponerPersonajeEnModoInvisible(idUser);
+						break;
+				}
+			} else {			
+				for (EscuchaCliente conectado : Servidor.getClientesConectados()) {
+					if (conectado.getIdPersonaje() != idUser) {
+						try {
+							conectado.getSalida().writeObject(gson.toJson(paqueteMensaje));
+						} catch (IOException e) {
+							Servidor.log.append("Falló al intentar enviar mensaje a:"
+									+ conectado.getPaquetePersonaje().getId() + ".\n");
+						}
 					}
 				}
+				Servidor.mensajeAAll(contador);
 			}
-			Servidor.mensajeAAll(contador);
 		}
+	}
+
+	private void ponerPersonajeEnModoInvisible(int idUser) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void ponerPersonajeEnModoExtraFuerza(int idUser) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void ponerPersonajeEnModoMitadFuerza(int idUser) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void permitirPersonajeAtravesarParedes(int idUser) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void ponerPersonajeEnModoDios(int idUser) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private int mensajeEsTruco(String mensaje) {
+		return trucos.indexOf(mensaje);
 	}
 }
